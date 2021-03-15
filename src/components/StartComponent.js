7b66bd198143c7e05898901729345e3e309de212
Danny Tran <tran.duy@husky.neu.edu>
2021-03-15 17:30:51 +0700

    Fixed async fetch bug

--- src/components/StartComponent.js
+++ src/components/StartComponent.js
@@ -40,67 +40,72 @@ export default class StartComponent extends React.Component {
     }
 
     componentDidMount() {
-        fetch('badgedata.json'
-            ,{
-                headers : {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }})
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    const error = new Error("Error fetching data");
-                    throw(error);
-                }
-            })
-            .then(r => {
-                console.log(r);
-                this.setState({
-                    badgeData: r
-                }, () => {
-                    this.updateBadge()
-                });
-            });
+        Promise.all([
+            fetch('badgedata.json'
+                , {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        const error = new Error("Error fetching data");
+                        throw (error);
+                    }
+                })
+                .then(r => {
+                    console.log(r);
+                    this.setState({
+                        badgeData: r
+                    }, () => {
+                        this.updateBadge()
+                    });
+                }),
 
-        fetch('questiondata.json'
-            ,{
-                headers : {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }})
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    const error = new Error("Error fetching data");
-                    throw(error);
-                }
-            })
-            .then(r => {
-                this.setState({
-                    questionData: r
+            fetch('questiondata.json'
+                , {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
                 })
-            })
-            .then(() => {
-                this.setState({
-                    curQuestion: this.state.questionData[0],
-                    curID: 0
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        const error = new Error("Error fetching data");
+                        throw (error);
+                    }
                 })
-            })
-            .then(() => this.updateBadge());
-        
-        fetch('runningtitle.txt')
-            .then(t => t.text())
-            .then(text => this.setState({ runningTitleData: text}))
-            .then(() => 
-            this.setState({
-                _isLoading: false
-            }));
+                .then(r => {
+                    this.setState({
+                        questionData: r
+                    })
+                })
+                .then(() => {
+                    this.setState({
+                        curQuestion: this.state.questionData[0],
+                        curID: 0
+                    })
+                })
+                .then(() => this.updateBadge()),
+
+            fetch('runningtitle.txt')
+                .then(t => t.text())
+                .then(text => this.setState({ runningTitleData: text }))
+        ])
+            .then(() =>
+                this.setState({
+                    _isLoading: false
+                }))
+
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        
+
     }
 
     setTime = (newTime) => {
@@ -111,7 +116,7 @@ export default class StartComponent extends React.Component {
 
     setBadge = newBadge => {
         console.log("adding badge : " + newBadge.name);
-        if(this.state.badge.findIndex(x => x.id === newBadge.id) === -1) {
+        if (this.state.badge.findIndex(x => x.id === newBadge.id) === -1) {
             this.setState(prevState => ({
                 badge: [...prevState.badge, newBadge]
             }))
@@ -119,10 +124,10 @@ export default class StartComponent extends React.Component {
     }
 
     replaceBadge = (newBadge) => {
-        if(this.state.badge.findIndex(x => x.id === newBadge.id) === -1) {
+        if (this.state.badge.findIndex(x => x.id === newBadge.id) === -1) {
             const array = [...this.state.badge];
             const oldBadge = this.state.badge.find(y => y.id === newBadge.upgrade);
-            if(oldBadge !== undefined) {
+            if (oldBadge !== undefined) {
                 array.splice(this.state.badge.indexOf(oldBadge), 1);
                 this.setState({
                     badge: array
@@ -139,7 +144,7 @@ export default class StartComponent extends React.Component {
         console.log("Called updateBadge")
         const earnedBadge = this.state.badgeData.find(b => this.state.curQuestion.badgeEarn.includes(b.id));
         if (earnedBadge !== undefined) {
-            if(earnedBadge.upgrade !== null) {
+            if (earnedBadge.upgrade !== null) {
                 this.replaceBadge(earnedBadge);
             } else {
                 this.setBadge(earnedBadge)
@@ -152,29 +157,29 @@ export default class StartComponent extends React.Component {
         console.log("fetching question: " + link)
         const fetchedQ = this.state.questionData.find(x => x.id === link)
         // this question does NOT require any badge
-        if(fetchedQ.requiredBadge.length === 0) {
+        if (fetchedQ.requiredBadge.length === 0) {
             this.setState({
                 curQuestion: this.state.questionData.find(x => x.id === link)
             }, () => {
                 this.updateBadge()
             })
-        // this question requires some badges and player has it
-        } else if (fetchedQ.requiredBadge.every(b => b >= 0 && this.state.badge.find(x => b===x.id) !== undefined)) {
+            // this question requires some badges and player has it
+        } else if (fetchedQ.requiredBadge.every(b => b >= 0 && this.state.badge.find(x => b === x.id) !== undefined)) {
             this.setState({
                 curQuestion: this.state.questionData.find(x => x.id === link)
             }, () => {
                 this.updateBadge()
             })
-        // this question requires the absence of some badges
-        } else if (fetchedQ.requiredBadge.every(b => b < 0 && this.state.badge.find(x => (0-b)===x.id) === undefined)) {
+            // this question requires the absence of some badges
+        } else if (fetchedQ.requiredBadge.every(b => b < 0 && this.state.badge.find(x => (0 - b) === x.id) === undefined)) {
             this.setState({
                 curQuestion: this.state.questionData.find(x => x.id === link)
             }, () => {
                 this.updateBadge()
             })
-        // player does NOT have the required badge or absence of badge
+            // player does NOT have the required badge or absence of badge
         } else {
-            if(fetchedQ.nextQ) {
+            if (fetchedQ.nextQ) {
                 this.fetchQuestion(fetchedQ.nextQ)
             } else {
                 this.fetchQuestion(fetchedQ.a1Link)
@@ -188,7 +193,7 @@ export default class StartComponent extends React.Component {
         }, () => this.fetchQuestion(link))
     }
 
-    turnOffForceUpdate = () => {this.setState({ forceUpdate: false })}
+    turnOffForceUpdate = () => { this.setState({ forceUpdate: false }) }
 
     restart = () => {
         this.setState({
@@ -200,108 +205,108 @@ export default class StartComponent extends React.Component {
     }
 
     render() {
-        return(
+        return (
             this.state._isLoading ?
-            <h1>Loading...</h1> :
-            <div className="E-body">
-                <span className="row mt-5 ">
-                    <span className="col-1"/>
-                    <span className="col-1">
-                        <img className="App-logo" alt="entropy logo" src={process.env.PUBLIC_URL + '/entropy-logo.png'} />
+                <h1>Loading...</h1> :
+                <div className="E-body">
+                    <span className="row mt-5 ">
+                        <span className="col-1" />
+                        <span className="col-1">
+                            <img className="App-logo" alt="entropy logo" src={process.env.PUBLIC_URL + '/entropy-logo.png'} />
+                        </span>
+                        <span className="col-7" />
+                        <span className="col-1">
+                            <h6>HOW TO PLAY</h6>
+                        </span>
+                        <span className="col-1">
+                            <h6>ABOUT US</h6>
+                        </span>
+                        <span className="col-1" />
                     </span>
-                    <span className="col-7"/>
-                    <span className="col-1">
-                        <h6>HOW TO PLAY</h6>
-                    </span>
-                    <span className="col-1">
-                        <h6>ABOUT US</h6>
-                    </span>
-                    <span className="col-1"/>
-                </span>
 
 
-                <span className="E-content row">
-                    <span className="col-1"/>
-                    <span className="row col-5">
-                        <span className="d-flex flex-column">
-                            <span className="">
-                                <RunningTitleComponent text={this.state.runningTitleData}/>
-                            </span>
-
-                            <span className="row justify-content-start">
-                                <span className="col-3">
-                                    <h6>TIME</h6>
-                                    <TimeComponent time={this.state.curQuestion.time}/>
+                    <span className="E-content row">
+                        <span className="col-1" />
+                        <span className="row col-5">
+                            <span className="d-flex flex-column">
+                                <span className="">
+                                    <RunningTitleComponent text={this.state.runningTitleData} />
                                 </span>
 
-                                <span className="col-9">
-                                    <h6>EVENTS</h6>
-                                    <QuestionComponent
-                                        key={this.state.curQuestion.id}
-                                        id={this.state.curQuestion.id}
-                                        restart={this.restart}
-                                        fetchQuestion={this.fetchQuestion}
-                                        question={this.state.curQuestion}
-                                        setTime={this.setTime}
-                                        setBadge={this.setBadge}/>
+                                <span className="row justify-content-start">
+                                    <span className="col-3">
+                                        <h6>TIME</h6>
+                                        <TimeComponent time={this.state.curQuestion.time} />
+                                    </span>
+
+                                    <span className="col-9">
+                                        <h6>EVENTS</h6>
+                                        <QuestionComponent
+                                            key={this.state.curQuestion.id}
+                                            id={this.state.curQuestion.id}
+                                            restart={this.restart}
+                                            fetchQuestion={this.fetchQuestion}
+                                            question={this.state.curQuestion}
+                                            setTime={this.setTime}
+                                            setBadge={this.setBadge} />
+                                    </span>
                                 </span>
                             </span>
                         </span>
+
+                        <span className="col-6" />
                     </span>
 
-                    <span className="col-6"/>
-                </span>
-
-                <span className="row">
-                    <span className="col-2"/>
-                    <span className="col-4">
-                        {
-                            this.state.curQuestion.nextQ === null &&
-                            this.state.curQuestion.id !== 2000 &&
-                            <span className="row">
-                                <span className="col-6 answer-box"
-                                      onClick={() => this.answerHandler(this.state.curQuestion.a1Link)}>
-                                    {this.state.curQuestion.a1}
+                    <span className="row">
+                        <span className="col-2" />
+                        <span className="col-4">
+                            {
+                                this.state.curQuestion.nextQ === null &&
+                                this.state.curQuestion.id !== 2000 &&
+                                <span className="row">
+                                    <span className="col-6 answer-box"
+                                        onClick={() => this.answerHandler(this.state.curQuestion.a1Link)}>
+                                        {this.state.curQuestion.a1}
+                                    </span>
+                                    <span className="col-6 answer-box"
+                                        onClick={() => this.answerHandler(this.state.curQuestion.a2Link)}>
+                                        {this.state.curQuestion.a2}
+                                    </span>
                                 </span>
-                                <span className="col-6 answer-box"
-                                      onClick={() => this.answerHandler(this.state.curQuestion.a2Link)}>
-                                    {this.state.curQuestion.a2}
+                            }
+                            {
+                                this.state.curQuestion.nextQ === null &&
+                                this.state.curQuestion.id === 2000 &&
+                                <div className="row">
+                                    <span className="col-12 answer-box"
+                                        onClick={() => this.restart()}>
+                                        {this.state.curQuestion.a1}
+                                    </span>
+                                </div>
+                            }
+                            {
+                                this.state.curQuestion.nextQ !== null &&
+                                <div className="row">
+                                    <span className="col-12 answer-box"
+                                        onClick={() => this.fetchQuestion(this.state.curQuestion.nextQ)}>
+                                        NEXT
                                 </span>
-                            </span>
-                        }
-                        {
-                            this.state.curQuestion.nextQ === null &&
-                            this.state.curQuestion.id === 2000 &&
-                            <div className="row">
-                                <span className="col-12 answer-box"
-                                      onClick={() => this.restart()}>
-                                    {this.state.curQuestion.a1}
-                                </span>
-                            </div>
-                        }
-                        {
-                            this.state.curQuestion.nextQ !== null &&
-                            <div className="row">
-                                <span className="col-12 answer-box"
-                                      onClick={() => this.fetchQuestion(this.state.curQuestion.nextQ)}>
-                                    NEXT
-                                </span>
-                            </div>
-                        }
+                                </div>
+                            }
+                        </span>
+                        <span className="col-6" />
                     </span>
-                    <span className="col-6"/>
-                </span>
 
-                <footer className="fixed-bottom E-antiscrolling">
-                    <BadgeListComponent 
-                        forceUpdate={this.state.forceUpdate}
-                        turnOffForceUpdate={this.turnOffForceUpdate}
-                        curQuestion={this.state.curQuestion.id}
-                        questionEarn={this.state.curQuestion.id}
-                        badge={this.state.badge}/>
-                </footer>
+                    <footer className="fixed-bottom E-antiscrolling">
+                        <BadgeListComponent
+                            forceUpdate={this.state.forceUpdate}
+                            turnOffForceUpdate={this.turnOffForceUpdate}
+                            curQuestion={this.state.curQuestion.id}
+                            questionEarn={this.state.curQuestion.id}
+                            badge={this.state.badge} />
+                    </footer>
 
-            </div>
+                </div>
         )
     }
 }
