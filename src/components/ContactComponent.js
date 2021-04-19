c9ba47fa55d5f7fb738a4c3ad8795e63ea96f8b2
Danny Tran <tran.duy@husky.neu.edu>
2021-04-19 16:10:50 +0700

    Updated contact page

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -6,14 +6,14 @@ export default class ContactComponent extends React.Component {
       <span className="E-contact-wrap">
         <div className="E-contact-content">
           <div id="Contact">
-            <svg class="Rectangle_40">
+            <svg className="Rectangle_40">
               <linearGradient spreadMethod="pad" id="LinearGradientFill1" x1="0.5" x2="0.5" y1="0" y2="1">
                 <stop offset="0" stop-color="#fff" stop-opacity="1"></stop>
                 <stop offset="0.1823" stop-color="#f0f0f0" stop-opacity="1"></stop>
                 <stop offset="0.8227" stop-color="#000" stop-opacity="1"></stop>
                 <stop offset="1" stop-color="#000" stop-opacity="1"></stop>
               </linearGradient>
-              <rect fill="url(#LinearGradientFill1)" id="Rectangle_40" rx="0" ry="0" x="0" y="0" width="2560" height="1436">
+              <rect fill="url(#LinearGradientFill1)" id="Rectangle_40" rx="0" ry="0" x="0" y="0" width="2357" height="1080">
               </rect>
             </svg>
             <div id="GET">
@@ -25,63 +25,59 @@ export default class ContactComponent extends React.Component {
             <div id="CONTACT">
               <span>CONTACT</span>
             </div>
-            <div id="Group_52">
-              <svg class="ID26195729_583036962051449_801">
-                <pattern elementId="ID26195729_583036962051449_801_A1_Rectangle_3" id="ID26195729_583036962051449_801_A1_Rectangle_3_pattern" x="0" y="0" width="100%" height="100%">
-                  <image x="0" y="0" width="100%" height="100%" href="trivupic.jpg" xlinkHref="ID26195729_583036962051449_801_A1_Rectangle_3_pattern.png"></image>
-                </pattern>
-                <rect fill="url(#ID26195729_583036962051449_801_A1_Rectangle_3_pattern)" id="ID26195729_583036962051449_801" rx="0" ry="0" x="0" y="0" width="366" height="366">
+            <svg class="ID26195729_583036962051449_801">
+              <pattern elementId="ID26195729_583036962051449_801_A1_Rectangle_3" id="ID26195729_583036962051449_801_A1_Rectangle_3_pattern" x="0" y="0" width="100%" height="100%">
+                <image x="0" y="0" width="100%" height="100%" href="trivupic.jpg" xlinkHref="ID26195729_583036962051449_801_A1_Rectangle_3_pattern.png"></image>
+              </pattern>
+              <rect fill="url(#ID26195729_583036962051449_801_A1_Rectangle_3_pattern)" id="ID26195729_583036962051449_801" rx="0" ry="0" x="0" y="0" width="260" height="260">
+              </rect>
+            </svg>
+            <div id="Group_18">
+              <svg class="Rectangle_53">
+                <rect fill="rgba(255,255,255,1)" id="Rectangle_53" rx="130" ry="130" x="0" y="0" width="674" height="260">
                 </rect>
               </svg>
-              <div id="Group_51">
-                <svg class="Rectangle_53">
-                  <rect fill="rgba(255,255,255,1)" id="Rectangle_53" rx="183" ry="183" x="0" y="0" width="939" height="366">
-                  </rect>
-                </svg>
-                <div id="Group_50">
-                  <div id="Tien_Vu">
-                    <span>Tien Vu</span>
-                  </div>
-                  <div id="Website___Behance___Instagram">
-                    <span>
-                      <a href="https://tienvu.format.com/" className="contact-link-style">Website</a> / 
-                      <a href="https://www.behance.net/trivu6198" className="contact-link-style"> Behance</a> / 
-                      <a href="https://www.instagram.com/xtienvux/" className="contact-link-style"> Instagram</a>
-                    </span>
-                  </div>
-                  <div id="Designer">
-                    <span>Designer</span>
-                  </div>
-                  <svg class="Line_1">
-                    <path fill="transparent" stroke="rgba(0,0,0,1)" stroke-width="2px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Line_1" d="M 0 0 L 557 0">
-                    </path>
-                  </svg>
+              <div id="Group_17">
+                <div id="Tien_Vu">
+                  <span>Tien Vu</span>
                 </div>
+                <div id="Website___Behance___Instagram">
+                  <span>Website / Behance / Instagram</span>
+                </div>
+                <div id="Designer">
+                  <span>Designer</span>
+                </div>
+                <svg class="Line_1">
+                  <path fill="transparent" stroke="rgba(0,0,0,1)" stroke-width="2px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Line_1" d="M 0 0 L 397.5881958007812 0">
+                  </path>
+                </svg>
+              </div>
+            </div>
+            <div id="Group_20">
+              <svg class="Rectangle_48">
+                <rect fill="rgba(0,0,0,1)" id="Rectangle_48" rx="130" ry="130" x="0" y="0" width="674" height="260">
+                </rect>
+              </svg>
+              <div id="Group_19">
+                <div id="Danny_Tran">
+                  <span>Danny Tran</span>
+                </div>
+                <div id="Website___Behance___Instagram_A1_Text_8">
+                  <span>Website / Behance / Instagram</span>
+                </div>
+                <div id="Developer">
+                  <span>Developer</span>
+                </div>
+                <svg class="Line_3">
+                  <path fill="transparent" stroke="rgba(255,255,255,1)" stroke-width="2px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Line_3" d="M 0 0 L 397.5881958007812 0">
+                  </path>
+                </svg>
               </div>
             </div>
             <svg class="Rectangle_51">
-              <rect fill="rgba(0,0,0,1)" id="Rectangle_51" rx="0" ry="0" x="0" y="0" width="366" height="366">
+              <rect fill="rgba(0,0,0,1)" id="Rectangle_51" rx="0" ry="0" x="0" y="0" width="260" height="260">
               </rect>
             </svg>
-            <div id="Group_53">
-              <svg class="Rectangle_48">
-                <rect fill="rgba(0,0,0,1)" id="Rectangle_48" rx="183" ry="183" x="0" y="0" width="939" height="366">
-                </rect>
-              </svg>
-              <div id="Danny_Tran">
-                <span>Danny Tran</span>
-              </div>
-              <div id="Website___Behance___Instagram_A1_Text_8">
-                <span>Website / Behance / Instagram</span>
-              </div>
-              <div id="Designer_A1_Text_9">
-                <span>Designer</span>
-              </div>
-              <svg class="Line_2">
-                <path fill="transparent" stroke="rgba(255,255,255,1)" stroke-width="2px" stroke-linejoin="miter" stroke-linecap="butt" stroke-miterlimit="4" shape-rendering="auto" id="Line_2" d="M 556 0 L 0 0">
-                </path>
-              </svg>
-            </div>
             <div id="Entropy_is_a_fictional_digital">
               <span>Entropy is a fictional digital platform meant to allow players to theorize alternate realities that could have happened as consequences of changing real historical events. Through fictional elements such as machine learning and generated simulations, it is intended to show a critical way of looking at history and explores ideas of determinism and multiple realities.</span>
             </div>
