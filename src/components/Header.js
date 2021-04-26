16ea3a7489de3e0b69db60e6d1e0fad5fb10ad7b
Danny Tran <tran.duy@husky.neu.edu>
2021-04-27 00:36:29 +0700

    Reverted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,16 @@ import {Link} from 'react-router-dom';
 
 function Header() {
   return(
-    <span className="row mt-5 ">
-        <span className="col-1" />
-        <span className="col-1">
+    <span className="d-flex ">
+        <span className="">
           <Link to='/start'>
             <img className="App-logo" alt="entropy logo" src={process.env.PUBLIC_URL + '/entropy-logo.png'} />
           </Link>
         </span>
-        <span className="col-7" />
-        <span className="col-1">
-            <Link to="/about"><h6>ABOUT</h6></Link>
-        </span>
-        <span className="col-1">
+        <span className="d-flex ml-auto">
+            <Link to="/about" className="pr-2"><h6>ABOUT</h6></Link>
             <Link to="/contact"><h6>CONTACT</h6></Link>
         </span>
-        <span className="col-1" />
     </span>)
 };
 
